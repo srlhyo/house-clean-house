b19f3f3f3a4608fc52a2731eea5cde63013d73c8
Helio Schultz <srlhyo@gmail.com>
2024-10-13 12:27:48 +0100

    Add ability to delete tasks

--- controllers/tasks/show.php
+++ controllers/tasks/show.php
@@ -9,15 +9,28 @@ $config = require base_path('config.php');
 
 $db = new Database($config, password: 'abc123');
 
-$task = $db->query("select * from tasks where id = :id", ["id" => $_GET["id"]])->findOrFail();
-
-// $heading = 'Task';
-
 $currentId = 3;
 
-authorize($task['user_id'] == $currentId);
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $task = $db->query("select * from tasks where id = :id", ["id" => $_GET["id"]])->findOrFail();
 
-view('tasks/show.view.php', [
-    'task' => $task,
-    'heading' => 'Task'
-]);
+    authorize($task['user_id'] == $currentId);
+
+    $db->query("delete from tasks where id = :id", [
+        "id" => $_POST['id'],
+    ]);
+
+    header('location: /tasks');
+    exit();
+} {
+
+    $task = $db->query("select * from tasks where id = :id", ["id" => $_GET["id"]])->findOrFail();
+    
+    authorize($task['user_id'] == $currentId);
+    
+    view('tasks/show.view.php', [
+        'task' => $task,
+        'heading' => 'Task'
+    ]);
+}
+
--- views/tasks/show.view.php
+++ views/tasks/show.view.php
@@ -6,7 +6,11 @@
 <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <a href="/tasks" class="underline text-blue-500">Go back...</a>
-        <p class="pt-6"><?= htmlspecialchars($task['description']) ?></p>
+        <p class="py-6"><?= htmlspecialchars($task['description']) ?></p>
+        <form action="" method="POST">
+            <input type="hidden" name="id" value=<?= $task['id']?>>
+            <button type="submit" class="text-xs text-red-500">Delete</button>
+        </form>
     </div>
 </main>
 
